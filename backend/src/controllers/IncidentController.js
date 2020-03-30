53e885680daa25eaab5470ecbf8e2a6027a4546c
Cristian Yamasato <cristian.t.c.y@gmail.com>
2020-03-30 20:26:05 -0300

    Correção bug cors

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,12 +3,13 @@ module.exports ={
     async index(request, response){
         const { page = 1 } =request.query;
         const [count] = await connection('incidents').count();
-
+        console.log(count);
         const incidents = await connection('incidents')
         .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page -1)*5)
         .select('incidents.*','ongs.name', 'ongs.email', 'ongs.city', 'ongs.uf');
+        response.header('X-total-Count', count['count(*)']);
         return  response.json(incidents);
         },
     async create(request, response){
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 // importando o modo express para dentro da variavel express
 const express = require('express'); 
 const routes =require('./routes');
-app.use(cors());
+const cors = require('cors');
 // variavel para armazenar a aplicação instanciano a aplicação
 const app = express();
+app.use(cors());
 // Converter o corpo da requisição em json
 app.use(express.json());
 app.use(routes);
